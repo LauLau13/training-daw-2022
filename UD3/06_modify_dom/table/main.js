d27766a1c7fa0c83b0060233d14052b1bc4923bf
Laura Gil <a25589svalero.com>
2022-10-24 18:27:31 +0200

    Ejercicios 24-10-2022

--- UD3/06_modify_dom/table/main.js
+++ UD3/06_modify_dom/table/main.js
@@ -1,9 +1,44 @@
+//1.Revisar, entender y comprender el código proporcionado:
+/*Respuesta:
+
+Cada vez que se hace click en el botón que llama a la función realiza lo siguiente:
 function onClick() {
+    //Detecta el elemento tabla
     let tbody = document.getElementsByTagName('tbody')[0]
 
+    //crea una fila tr y añade en la 1a columna el texto '0',
     let tr = document.createElement('tr')
     let td = document.createElement('td')
     td.innerText = '0'
+    tr.appendChild(td)
+
+    //añade en la segunda columna el nombre 'Example'
+    td = document.createElement('td')
+    td.innerText = 'Example'
+    tr.appendChild(td)
+
+    //añade en la última columna el Surname 'Surname'
+    td = document.createElement('td')
+    td.innerText = 'Surname'
+    tr.appendChild(td)
+
+    //Llama a la línea recien creada y la imprime por pantalla
+    tbody.appendChild(tr)
+    console.log('add')
+}
+*/
+
+//2.Hacer que el ID se autoincremental (mediante una variable)
+//3. Añadir una columna “Acciones” en el HTML
+/*4.Añadir, a los datos que se incluyen cada que se pulsa el botón, 
+un enlace a la url ‘http://localhost/edit/id*/
+let value = 1;
+
+function onClick() {
+    let tbody = document.getElementsByTagName('tbody')[0]
+    let tr = document.createElement('tr')
+    let td = document.createElement('td')
+    td.innerText = value ++;
     tr.appendChild(td)
 
     td = document.createElement('td')
@@ -12,6 +47,13 @@ function onClick() {
 
     td = document.createElement('td')
     td.innerText = 'Surname'
+    tr.appendChild(td)
+
+    td = document.createElement('td')
+    a = document.createElement('a')
+    a.innerText = 'enlace'
+    a.href = 'http://localhost/edit/' + current
+    td.appendChild(a)
     tr.appendChild(td)
 
     tbody.appendChild(tr)
