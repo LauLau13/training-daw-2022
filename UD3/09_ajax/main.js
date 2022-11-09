cc40eddf219743617c0312eb8c39f4496185e74f
Laura Gil <a25589svalero.com>
2022-11-09 19:36:52 +0100

    UD3/09 corregido

--- UD3/09_ajax/main.js
+++ UD3/09_ajax/main.js
@@ -1,14 +1,51 @@
-window.onload = function() {
-    console.log('document loaded');
+/*
+Generar una página que, al pulsar el botón cargar datos, 
+realice una petición a la url http://dummy.restapiexample.com/api/v1/employee para obtener todos los empleados.
 
-    var xhttp = new XMLHttpRequest();   
+Por cada empleado, insertará una fila en una tabla
+*/
 
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            console.log(this.responseText)
-        }
-      };
+window.onload = function () {
+  console.log("document loaded");
 
-    xhttp.open("GET", "http://dummy.restapiexample.com/api/v1/employees", true);
-    xhttp.send();
+  var xhttp = new XMLHttpRequest();
+  let employees;
+
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      console.log(this.responseText);
+      employees = JSON.parse(this.responseText).data;
+
+      for (let i = 0; i < employees.length; i++) {
+        addRow(employees[i]);
+      }
+    }
+  };
+
+  xhttp.open("GET", "http://dummy.restapiexample.com/api/v1/employees", true);
+  xhttp.send();
+};
+
+function addRow(employee) {
+  let tbody = document.getElementById("tbody");
+  let tr = document.createElement("tr");
+
+  td = document.createElement("td");
+  td.innerText = employee.id;
+  tr.appendChild(td);
+
+  td = document.createElement("td");
+  td.innerText = employee.employee_name;
+  tr.appendChild(td); 
+
+  td = document.createElement("td");
+  td.innerText = employee.employee_salary;
+  tr.appendChild(td);
+
+  td = document.createElement("td");
+  td.innerText = employee.employee_age;
+  tr.appendChild(td);
+
+  tbody.appendChild(tr);
+  console.log("add");
 }
