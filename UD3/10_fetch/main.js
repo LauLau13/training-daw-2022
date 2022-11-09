ad54b3de98181ef2e14eecdc0d49e5d5f742f333
Laura Gil <a25589svalero.com>
2022-11-09 19:37:52 +0100

    UD3/10 corregido

--- UD3/10_fetch/main.js
+++ UD3/10_fetch/main.js
@@ -1,7 +1,7 @@
 window.onload = function() {
     console.log('document loaded');
 
-    fetch('http://dummy.restapiexample.com/api/v1/employees')
+    fetch('http://dummy.restapiexample.com/api/v1/employees.json')
         .then(response => response.json())
         .then(response => {
             console.log(response)
